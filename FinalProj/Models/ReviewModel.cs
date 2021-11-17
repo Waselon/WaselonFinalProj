ae2d4212307c3ea447d04314668bb710760e22d5
Oleksii Bryheda <waselon95@gmail.com>
2021-11-17 17:49:52 +0200

    added models

---
+++ FinalProj/Models/ReviewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProj.Models
+{
+    public class Review
+    {
+        public string Group { get; set; }
+        public string Text { get; set; }
+        public byte Mark { get; set; }
+        public float Rating { get; set; }
+    }
+}
---
+++ FinalProj/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProj.Models;
+
+namespace FinalProj.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<Review> Reviews { get; set; }
+    }
+}
